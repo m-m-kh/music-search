50f322f52de2bc76f6f9105e33570bc38b164a8a
m.m.kh <50243954+m-m-kh@users.noreply.github.com>
2022-12-07 16:33:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram.ext import CallbackContext ,Updater, MessageHandler, Filters
 from telegram import Update, InlineKeyboardMarkup,InlineKeyboardButton
 from tests.test import findmusic
 import os
-updater = Updater('5567723428:AAGqfiF5XlZaC6r0KcBd_rKZZiLfGvxQOCM')
+updater = Updater('5567723428:AAHfA4UJdJdzG1tBqmNe3xXXOZYTxADHI8I')
 
 def search(update:Update, cnotext:CallbackContext):
     result = findmusic(f'آهنگ {update.message.text}')
